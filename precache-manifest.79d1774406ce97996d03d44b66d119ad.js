ab6186bb5f96e39f19dc257198871c334f001be2
lokises <lokises@mcircle.biz>
2020-04-25 17:09:11 +0900

    Updates

---
+++ precache-manifest.79d1774406ce97996d03d44b66d119ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c044426ba93529af69689de51bb68146",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "1296ded0d12d5b49f31c",
+    "url": "/weather-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3f1fe1c6e7a4b9e3a7db",
+    "url": "/weather-react/static/js/2.e80b7e4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react/static/js/2.e80b7e4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1296ded0d12d5b49f31c",
+    "url": "/weather-react/static/js/main.cddcd2b7.chunk.js"
+  },
+  {
+    "revision": "69d2dc459bdaf641288a",
+    "url": "/weather-react/static/js/runtime-main.aad04959.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
