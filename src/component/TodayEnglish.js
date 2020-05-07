8f4805ea76d630e6b3f039ab77c0c8e35ea56d34
lokises <lokises@mcircle.biz>
2020-05-07 19:16:03 +0900

    오늘의 영어 페이지 작성 중

--- src/component/TodayEnglish.js
+++ src/component/TodayEnglish.js
@@ -1,14 +1,28 @@
-import React, {Fragment} from "react";
-import '../css/English.css'
+import React, { Fragment } from "react";
+
+import "../css/English.css";
 
 const TodayEnglish = () => {
     return (
         <Fragment>
-            <div className='englishDiv'>
-                <span>english here</span>
+            <div className="englishDiv">
+                <div className="englishHeadfContainer">
+                    <h2>something img</h2>
+                </div>
+                <div className="englishCenterContainer">
+                    <textarea className="englishTextArea" placeholder="오늘의 영문장을 작성하세요"></textarea>
+                    <div className="englishBtnDiv">
+                        <button className="englishBtn">글 지우기</button>
+                        <button className="englishBtn">글 등록</button>
+                    </div>
+                </div>
+                <div className="englishBottomContainer">
+                    <span>title</span>
+                    <span>subtitle</span>
+                </div>
             </div>
         </Fragment>
     );
-}
+};
 
 export default TodayEnglish;
