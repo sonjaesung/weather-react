fa37f0de1e30b0ddfbfd54b0c1c1893ea4252a38
lokises <lokises@mcircle.biz>
2020-05-15 17:30:38 +0900

    className modify

--- src/component/Bucketlist.js
+++ src/component/Bucketlist.js
@@ -4,22 +4,29 @@ import "../css/BucketList.css";
 
 const Bucketlist = () => {
     useEffect(() => {
-        let headerTitle = document.querySelector('.headTitle');
-        headerTitle.innerText = '버킷리스트';
+        let headerTitle = document.querySelector(".headTitle");
+        headerTitle.innerText = "버킷리스트";
     });
 
     return (
         <Fragment>
             <div className="bucketListDiv">
-                <div className='bucketHeadContainer'></div>
-                <div className='bucketCenterContainer'>
-                    <input type="file" id="real-input" class="image_inputType_file" accept="img/*" required multiple />
-                    <button class="browse-btn">사진업로드</button>
+                <div className="bucketHeadContainer"></div>
+                <div className="bucketCenterContainer">
+                    <input
+                        type="file"
+                        id="real-input"
+                        className="image_inputType_file"
+                        accept="img/*"
+                        required
+                        multiple
+                    />
+                    <button className="browse-btn">사진업로드</button>
                 </div>
-                <div className='bucketBottomContainer'></div>
+                <div className="bucketBottomContainer"></div>
             </div>
         </Fragment>
-    )
-}
+    );
+};
 
 export default Bucketlist;
