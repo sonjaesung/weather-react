a626fe476d14c6f6c2d3272e327aea4b3a7ea46b
sonjaesung <loki92@naver.com>
2020-05-22 16:04:32 +0900

    join validation, css

--- src/component/Join.js
+++ src/component/Join.js
@@ -8,14 +8,72 @@ import "../css/Join.css";
         headerTitle.innerText = '회원가입';
     });
 
+    const clickJoinBtn = () => {
+        let email = document.getElementById('joinEmail').value;
+        let pw = document.getElementById('joinPw').value;
+        let pwAgain = document.getElementById('joinPwCheck').value;
+        let name = document.getElementById('joinName').value;
+        let phone = document.getElementById('joinPhone').value;
+        let genderArray = document.getElementsByName('gender');
+        let gender;
+
+        genderArray.forEach(data => {
+            if(data.checked === true)
+            {
+                gender = data.value;
+            }
+        });
+
+        if(!validation(email, pw, pwAgain, name, phone, gender))
+        {
+            return ;
+        }   
+    }
+
+    const validation = (email, pw, pwAgain, name, phone, gender) => {
+        if(email === "")
+        {
+            alert('email을 입력해 주세요.');
+            return false;
+        }
+        else if(pw === "")
+        {
+            alert('비밀번호를 입력해 주세요.');
+            return false;
+        }
+        else if(pw !== pwAgain)
+        {
+            alert('비밀번호가 일치하지않습니다.');
+            return false;
+        }
+        else if(name === "")
+        {
+            alert('이름을 입력해 주세요.');
+            return false;
+        }
+        else if(phone === "")
+        {
+            alert('전화번호를 입력해 주세요.');
+            return false;
+        }
+        else if(gender === undefined)
+        {
+            alert('성별을 정해주세요.');
+            return false;
+        }
+
+        return true;
+    }
+
     return (
         <Fragment>
             <div className='joinDiv'>
                 <div className='joinContainerTop'>
-                    <input placeholder='E-Mail을 입력하세요' type='email' className='joinInput' />
-                    <input placeholder='비밀번호를 입력하세요' type='passwo' className='joinInput' />
-                    <input placeholder='이름을 입력하세요' type='text' className='joinInput' />
-                    <input placeholder='전화버호을 입력하세요' type='text' className='joinInput' />
+                    <input placeholder='E-Mail을 입력하세요' type='email' className='joinInput' id='joinEmail' />
+                    <input placeholder='비밀번호를 입력하세요' type='password' className='joinInput' id='joinPw' />
+                    <input placeholder='비밀번호를 확인하세요' type='password' className='joinInput' id='joinPwCheck' />
+                    <input placeholder='이름을 입력하세요' type='text' className='joinInput' id='joinName' />
+                    <input placeholder='전화버호를 (-)없이 입력하세요' type='number' className='joinInput' id='joinPhone' />
                     <div className='genderDiv'>
                         <fieldset className='genderFieldset'>
                         <legend>성별</legend>
@@ -28,7 +86,11 @@ import "../css/Join.css";
                         </fieldset>
                     </div>
                 </div>
-                <div className='joinContainerBottom'> </div>
+                <div className='joinContainerBottom'>
+                    <button className='joinBtn' onClick={clickJoinBtn}>
+                        완료
+                    </button>
+                </div>
             </div>
         </Fragment>
     );
